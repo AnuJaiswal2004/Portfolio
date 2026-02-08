002d9e7ab4a0c1c93bba8f0168f9bdb930c2b99a
Anubhav <anubhavjaiswal37@gmail.com>
2026-02-09 00:04:58 +0530

    Added mulltiple sections

--- src/Components/ThemeToggle.jsx
+++ src/Components/ThemeToggle.jsx
@@ -7,6 +7,7 @@ export const ThemeToggle = () => {
 
   useEffect(() => {
     const storedTheme = localStorage.getItem("theme");
+
     if (storedTheme === "dark") {
       setIsDarkMode(true);
       document.documentElement.classList.add("dark");
@@ -32,8 +33,7 @@ export const ThemeToggle = () => {
     <button
       onClick={toggleTheme}
       className={cn(
-        "fixed max-sm:hidden top-5 right-5 z-50 p-2 rounded-full transition-colors duration-300",
-        "focus:outlin-hidden"
+        "fixed max-sm:hidden top-5 right-5 z-50 p-2 rounded-full transition-colors duration-300 focus:outline-none"
       )}
     >
       {isDarkMode ? (
