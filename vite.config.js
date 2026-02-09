54a487a0c09f2f3f488db3de8bbc60930851d7d8
Anubhav <anubhavjaiswal37@gmail.com>
2026-02-09 22:49:28 +0530

    the new version with @import 'tailwindcss'

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],   // ❌ removed tailwind plugin
+  plugins: [react(), tailwindcss()],  // ✅ Add tailwindcss() here
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
